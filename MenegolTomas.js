197f504b1cb052ce21b61aa21991271982652cd8
Tomas Menegol <tomas.menegol@gmail.com>
2022-09-29 07:14:48 -0300

    entrega Class

--- MenegolTomas.js
+++ MenegolTomas.js
@@ -1,1 +1,46 @@
-//Actividad coderhouse: 
+class Usuario {
+    constructor(nombre, apellido){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.libros = []
+        this.mascotaS = []
+    }
+    getFullName(){
+        console.log(`Hola soy ${this.nombre} ${this.apellido}`);
+    }
+    addMascota(mascota){
+        this.mascotaS.push(`${mascota}`)
+    }
+    countMascotas(){
+        console.log(this.mascotaS.length)
+    }
+    addBook(titulo, escritor){
+        this.libros.push({nombre: titulo, autor: escritor})
+    }
+    getBookNames(){
+        console.log(this.libros.map((libritos) => libritos.nombre ))
+    }
+}
+
+prueba = new Usuario("tom", "menegol")
+
+prueba.getFullName()
+
+prueba.addMascota("michi")
+
+prueba.addMascota("firulai")
+
+console.log(prueba.mascotaS);
+
+prueba.countMascotas()
+
+prueba.addBook("harry potter","JK Rowling")
+
+prueba.addBook("dune","Frank Helbert")
+
+console.log(prueba.libros);
+
+prueba.getBookNames()
+
+
+

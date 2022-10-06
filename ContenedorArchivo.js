7900a999ecfae130054aaf3b6d5881a464e905c3
Tomas Menegol <tomas.menegol@gmail.com>
2022-10-06 10:18:01 -0300

    error de id nada mas

--- ContenedorArchivo.js
+++ ContenedorArchivo.js
@@ -5,36 +5,33 @@ class Contenedor {
         this.nombreArchivo = nombreArchivo
         const ruta = `./${this.nombreArchivo}.JSON`
         const data = "[]"
-        fs.writeFile(ruta, data, (err, output) => {
+        fs.promises.
+            writeFile(ruta, data)
+            .then(()=> {console.log(`Tu archivo ${this.nombreArchivo} se creo correctamente`)})
+            .catch((err)=> {console.log("Fallo la creacion del archivo: " + err)}) 
+    }
+    save(objeto){
+        const ruta = `./${this.nombreArchivo}.JSON`
+        fs.readFile(ruta, "utf-8", (err, output) =>{
             if(err){
-                console.log("Fallo la creacion del archivo: " + err)
+                console.log("Fallo la lectura del archivo: " + err)
             }else{
-                console.log(`Tu archivo ${this.nombreArchivo} se creo correctamente`)
+                const contenido = JSON.parse(output)
+                ultimoId = contenido[contenido.lenght -1].id
+                ultimoId ? objeto.id = ultimoId + 1 : objeto.id = 1
+                nuevoContenido = contenido.push(objeto)
+                contenidoGuardado = JSON.stringify(nuevoContenido)
+                fs.writeFile(ruta, contenidoGuardado, (err, output) => {
+                    if(err){
+                        console.log("Error al guardar el objeto: " + err)
+                    }else{
+                        console.log(`Se guardo tu objeto con id ${objeto.id}`);
+                    }
+                })
+
             }
         })
     }
-    // save(objeto){
-    //     const ruta = `./${this.nombreArchivo}.JSON`
-    //     fs.readFile(ruta, "utf-8", (err, output) =>{
-    //         if(err){
-    //             console.log("Fallo la lectura del archivo: " + err)
-    //         }else{
-    //             const contenido = JSON.parse(output)
-    //             ultimoId = contenido[contenido.lenght -1].id
-    //             ultimoId ? objeto.id = ultimoId + 1 : objeto.id = 1
-    //             nuevoContenido = contenido.push(objeto)
-    //             contenidoGuardado = JSON.stringify(nuevoContenido)
-    //             fs.writeFile(ruta, contenidoGuardado, (err, output) => {
-    //                 if(err){
-    //                     console.log("Error al guardar el objeto: " + err)
-    //                 }else{
-    //                     console.log(`Se guardo tu objeto con id ${objeto.id}`);
-    //                 }
-    //             })
-
-    //         }
-    //     })
-    // }
     getById(number){
         
     }
@@ -51,4 +48,4 @@ class Contenedor {
 
 prueba = new Contenedor("productos")
 
-// setTimeout(prueba.save({titulo: "producto1", price: 123}), 1000)
+prueba.save({titulo: "producto1", price: 123})
